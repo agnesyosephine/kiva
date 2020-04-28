5118e3fe5753633f864c472301f9b293c3336a20
agnesyosephine <agnesyosephine4@gmail.com>
2020-04-28 17:22:01 +0800

    28/4

--- 24-3 (return pod)/assignmentOP.py
+++ 24-3 (return pod)/assignmentOP.py
@@ -6,11 +6,12 @@ def assignOP():
     order_path = os.getcwd() + "\orders.csv"
     pod_path = os.getcwd() + "\item in pod.csv"
     assigned_path = os.getcwd() + "\Assigned_order_to_pod.csv"
+    selected_pod = pd.read_csv(assigned_path, names = ["id", "due_date"])
     data_order = pd.read_csv(order_path, names = ["item", "qty", "due_date"])
-    data_pod = pd.read_csv(pod_path, names = ["pod_id", "item", "qty", "due_date"])
+    data_pod = pd.read_csv(pod_path, names = ["pod_id", "item", "qty", "due_date", "max_qty"])
     data_order = data_order.sort_values(by=['due_date','qty'])
     data_order = data_order.reset_index(drop=True)
-    data_pod = data_pod.sort_values(by=['qty'])
+    data_pod = data_pod.sort_values(by=['due_date','qty'])
     data_pod = data_pod.reset_index(drop=True)
     no_match = []
     opsiPod = []
@@ -20,13 +21,18 @@ def assignOP():
         for i in range (len(data_pod.item)):
             if data_pod.item[i] == data_order.item[j] and data_pod.qty[i] >= data_order.qty[j]:
                 data_pod.qty[i] -= data_order.qty[j]
-                opsiPod.append(data_pod.pod_id[i])
+                data_pod.due_date[i] = data_order.due_date[j]
+                selected_pod.loc[len(selected_pod)] = [data_pod.pod_id[i],data_pod.due_date[i]]
                 break;
         if j == (len(data_pod.item)-1):
             no_match.append(j)
             notAssigned.append([data_order.item[j],data_order.qty[j],data_order.due_date[j]])
 
-    result = opsiPod
+    selected_pod = selected_pod.sort_values(by= ['due_date'])
+    selected_pod = selected_pod.reset_index(drop=True)
+
+    result = selected_pod.id.values.tolist()
+    np.savetxt(assigned_path,selected_pod, delimiter = ',')
     np.savetxt(order_path, notAssigned, delimiter = ',')
     np.savetxt(pod_path, data_pod, delimiter = ',')  
     return result   
