3080f13283c081e99c48ac194f6fbddf32728a7e
Pranacahya <pranacahya_13@yahoo.com>
2020-08-18 13:25:19 +0800

    new initiation

---
+++ merged_baseline/checkPO.py
@@ -0,0 +1,21 @@
+def CheckPriorityOrder(time,podlist):
+    import pandas as pd
+    import os
+    import numpy as np
+
+    assigned_path = os.getcwd() + "\Assigned_order_to_pod.csv"
+    selected_pod = pd.read_csv(assigned_path, names = ["id","due_date","qty","time","finish_time"])
+    priorityOrder = []
+    for j in range (len(selected_pod.id)):
+        if selected_pod.finish_time[j] == 0 and (selected_pod.due_date[j] - (time/3600)) < 1:
+            priorityOrder.append(selected_pod.id[j]) 
+
+    if len(priorityOrder) == 0:
+        result = 999
+    else:
+        for i in range (len(priorityOrder)):
+            if priorityOrder[i] in podlist:
+                result = priorityOrder[i]
+            else:
+                result = 999   
+    return result   
